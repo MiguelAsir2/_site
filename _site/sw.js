920be6f26fb247eafbaec838f46ec81af17735f3
Miguel <miguel@MCA>
2022-03-23 20:46:59 +0100

    Despliegue Actualizado

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220324.0344';
+const cacheName = 'chirpy-20220324.0346';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
